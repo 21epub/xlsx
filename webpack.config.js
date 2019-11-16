88f75e671e27315d28f92c1e4ae67498e0e2ec94
garrygu <garry.guzy@gmail.com>
2019-11-16 15:15:07 +0800

    fix: 在项目中存在无法粘贴Excel内容的问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+
 module.exports = {
   entry: './app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     library: "xlsx",
-    libraryTarget: "umd"
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
